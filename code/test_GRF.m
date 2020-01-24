89238bb3387e96705d350086a4281b2541c6dbc0
Lucas Veras <lucasdsveras@gmail.com>
2020-01-24 18:24:07 +0000

    Rename platform 1 data variables

--- code/test_GRF.m
+++ code/test_GRF.m
@@ -32,17 +32,17 @@ data = dlmread(path_to_file);
 
 % Retrieve data from platform 1
 % Ground reaction force (GRF; N)
-[fX, fY, fZ] = deal(data(:, 1), data(:, 2), data(:, 3));
-fR = sqrt(fX.^2 + fY.^2 + fZ.^2); % compute resultant vector
+[fX1, fY1, fZ1] = deal(data(:, 1), data(:, 2), data(:, 3));
+fR1 = sqrt(fX1.^2 + fY1.^2 + fZ1.^2); % compute resultant vector
 time = 1:length(data);
 time = time / samp_freq;  % time in seconds
 % Get GRF in BW
 g = 9.81;  % gravity acceleration (m/s2)
 body_weight = body_mass * g;  % body weight (BW; N)
-fX_BW = fX / body_weight;
-fY_BW = fY / body_weight;
-fZ_BW = fZ / body_weight;
-fR_BW = fR / body_weight;
+fX1_BW = fX1 / body_weight;
+fY1_BW = fY1 / body_weight;
+fZ1_BW = fZ1 / body_weight;
+fR1_BW = fR1 / body_weight;
 
 % Filter GRF data
 % Create the lowpass filter
@@ -54,95 +54,95 @@ Wn = cutoff / fnyq;  % Filter parameter
 [b, a] = butter(n, Wn, 'low');
 
 % Filter GRF (N)
-fX = filtfilt(b, a, fX);
-fY = filtfilt(b, a, fY);
-fZ = filtfilt(b, a, fZ);
-fR = filtfilt(b, a, fR);
+fX1 = filtfilt(b, a, fX1);
+fY1 = filtfilt(b, a, fY1);
+fZ1 = filtfilt(b, a, fZ1);
+fR1 = filtfilt(b, a, fR1);
 % Filter GRF (BW)
-fX_BW = filtfilt(b, a, fX_BW);
-fY_BW = filtfilt(b, a, fY_BW);
-fZ_BW = filtfilt(b, a, fZ_BW);
-fR_BW = filtfilt(b, a, fR_BW);
+fX1_BW = filtfilt(b, a, fX1_BW);
+fY1_BW = filtfilt(b, a, fY1_BW);
+fZ1_BW = filtfilt(b, a, fZ1_BW);
+fR1_BW = filtfilt(b, a, fR1_BW);
 
 % Find peaks
-height_fZ = 3 * mean(fZ); % Vertical GRF (N)
-height_fR = 3 * mean(fR); % Resultant GRF (N)
-height_fZ_BW = 3 * mean(fZ_BW); % Vertical GRF (BW)
-height_fR_BW = 3 * mean(fR_BW); % Resultant GRF (BW)
+height_fZ1 = 3 * mean(fZ1); % Vertical GRF (N)
+height_fR1 = 3 * mean(fR1); % Resultant GRF (N)
+height_fZ1_BW = 3 * mean(fZ1_BW); % Vertical GRF (BW)
+height_fR1_BW = 3 * mean(fR1_BW); % Resultant GRF (BW)
 h_dist = 0.2 * samp_freq;  % seconds * sampling frequency
 
 % Peak GRF (N)
-[pks_fZ, locs_fZ] = findpeaks(fZ, 'MINPEAKHEIGHT', height_fZ, 'MINPEAKDISTANCE', h_dist);
-[pks_fR, locs_fR] = findpeaks(fR, 'MINPEAKHEIGHT', height_fR, 'MINPEAKDISTANCE', h_dist);
-time_of_peaks_fZ = locs_fZ / samp_freq;
-time_of_peaks_fR = locs_fR / samp_freq;
+[pks_fZ1, locs_fZ1] = findpeaks(fZ1, 'MINPEAKHEIGHT', height_fZ1, 'MINPEAKDISTANCE', h_dist);
+[pks_fR1, locs_fR1] = findpeaks(fR1, 'MINPEAKHEIGHT', height_fR1, 'MINPEAKDISTANCE', h_dist);
+time_of_peaks_fZ1 = locs_fZ1 / samp_freq;
+time_of_peaks_fR1 = locs_fR1 / samp_freq;
 % Peak GRF (BW)
-[pks_fZ_BW, locs_fZ_BW] = findpeaks(fZ_BW, 'MINPEAKHEIGHT', height_fZ_BW, 'MINPEAKDISTANCE', h_dist);
-[pks_fR_BW, locs_fR_BW] = findpeaks(fR_BW, 'MINPEAKHEIGHT', height_fR_BW, 'MINPEAKDISTANCE', h_dist);
-time_of_peaks_fZ_BW = locs_fZ_BW / samp_freq;
-time_of_peaks_fR_BW = locs_fR_BW / samp_freq;
+[pks_fZ1_BW, locs_fZ1_BW] = findpeaks(fZ1_BW, 'MINPEAKHEIGHT', height_fZ1_BW, 'MINPEAKDISTANCE', h_dist);
+[pks_fR1_BW, locs_fR1_BW] = findpeaks(fR1_BW, 'MINPEAKHEIGHT', height_fR1_BW, 'MINPEAKDISTANCE', h_dist);
+time_of_peaks_fZ1_BW = locs_fZ1_BW / samp_freq;
+time_of_peaks_fR1_BW = locs_fR1_BW / samp_freq;
 
 
 % Plot Vertical GRF (N) x Time (s)
 figure('NAME', 'Vertical GRF (N) x Time (s)')
 set(gcf, 'Position', get(0, 'Screensize'));
-h11 = plot(time, fZ);
+h11 = plot(time, fZ1);
 grid on
 hold on
-h12 = plot(time_of_peaks_fZ, pks_fZ, 'x', 'MarkerSize', 10);
+h12 = plot(time_of_peaks_fZ1, pks_fZ1, 'x', 'MarkerSize', 10);
 xlabel('Time (s)', 'FontSize', 20)
 ylabel('Vertical ground reaction force (N)', 'FontSize', 20)
 xticks(0:5:max(time));
-yticks(0:500:(ceil(max(fZ) / 500) * 500));
+yticks(0:500:(ceil(max(fZ1) / 500) * 500));
 xlim([0 max(time)]);
-ylim([0 (ceil(max(fZ) / 500) * 500)]);
+ylim([0 (ceil(max(fZ1) / 500) * 500)]);
 ax = gca;
 ax.FontSize = 16;
 
 % Plot Vertical GRF (BW) x Time (s)
 figure('NAME', 'Vertical GRF (BW) x Time (s)')
 set(gcf, 'Position', get(0, 'Screensize'));
-h21 = plot(time, fZ_BW);
+h21 = plot(time, fZ1_BW);
 grid on
 hold on
-h22 = plot(time_of_peaks_fZ_BW, pks_fZ_BW, 'x', 'MarkerSize', 10);
+h22 = plot(time_of_peaks_fZ1_BW, pks_fZ1_BW, 'x', 'MarkerSize', 10);
 xlabel('Time (s)', 'FontSize', 20)
 ylabel('Vertical ground reaction force (BW)', 'FontSize', 20)
 xticks(0:5:max(time));
-yticks(0:0.5:ceil(max(fZ_BW)));
+yticks(0:0.5:ceil(max(fZ1_BW)));
 xlim([0 max(time)]);
-ylim([0 ceil(max(fZ_BW))]);
+ylim([0 ceil(max(fZ1_BW))]);
 ax = gca;
 ax.FontSize = 16;
 
 % Plot Resultant GRF (N) x Time (s)
 figure('NAME', 'Resultant GRF (N) x Time (s)')
 set(gcf, 'Position', get(0, 'Screensize'));
-h11 = plot(time, fR);
+h11 = plot(time, fR1);
 grid on
 hold on
-h12 = plot(time_of_peaks_fR, pks_fR, 'x', 'MarkerSize', 10);
+h12 = plot(time_of_peaks_fR1, pks_fR1, 'x', 'MarkerSize', 10);
 xlabel('Time (s)', 'FontSize', 20)
 ylabel('Resultant ground reaction force (N)', 'FontSize', 20)
 xticks(0:5:max(time));
-yticks(0:500:(ceil(max(fR) / 500) * 500));
+yticks(0:500:(ceil(max(fR1) / 500) * 500));
 xlim([0 max(time)]);
-ylim([0 (ceil(max(fR) / 500) * 500)]);
+ylim([0 (ceil(max(fR1) / 500) * 500)]);
 ax = gca;
 ax.FontSize = 16;
 
 % Plot Resultant GRF (BW) x Time (s)
 figure('NAME', 'Resultant GRF (BW) x Time (s)')
 set(gcf, 'Position', get(0, 'Screensize'));
-h11 = plot(time, fR_BW);
+h11 = plot(time, fR1_BW);
 grid on
 hold on
-h12 = plot(time_of_peaks_fR_BW, pks_fR_BW, 'x', 'MarkerSize', 10);
+h12 = plot(time_of_peaks_fR1_BW, pks_fR1_BW, 'x', 'MarkerSize', 10);
 xlabel('Time (s)', 'FontSize', 20)
 ylabel('Resultant ground reaction force (BW)', 'FontSize', 20)
 xticks(0:5:max(time));
-yticks(0:0.5:ceil(max(fR_BW)));
+yticks(0:0.5:ceil(max(fR1_BW)));
 xlim([0 max(time)]);
-ylim([0 ceil(max(fR_BW))]);
+ylim([0 ceil(max(fR1_BW))]);
 ax = gca;
 ax.FontSize = 16;
