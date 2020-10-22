bd69ad97addba110cb4deb3509dbb339c09d2b92
Lucas <lucasdsveras@gmail.com>
2020-10-22 19:50:06 +0100

    Document function

--- code/functions/getEvalEnd.m
+++ code/functions/getEvalEnd.m
@@ -1,17 +1,21 @@
-function endEval = getEvalEnd(vector)
-% getEvalEnd gets the array index corresponding to the end of the evaluation.
+function endEval = getEvalEnd(signal)
+% GETEVALEND gets the array index corresponding to the end of the evaluation.
 % The end is assumed when there are at least two consecutive seconds of 0
 % standard deviation.
 %
-% vector should be an array with the ground reaction force vector signal.
+% Input arguments:
+% 	signal: a double array with the signal.
+%
+% Output arguments:
+% 	endEval: a double scalar with the index where the evaluation ends.
 
 	blockSize = 1000; % 1 sec * 1000 Hz samp_freq
-	nBlocks = size(vector, 1) / blockSize;
+	nBlocks = size(signal, 1) / blockSize;
 	dataSd = zeros(nBlocks, 1);
 	for i = 1:nBlocks
 		startIdx = i * blockSize - (blockSize - 1);
 		endIdx = i * blockSize;
-		dataSd(i) = std(vector(startIdx:endIdx));
+		dataSd(i) = std(signal(startIdx:endIdx));
 	end
 
 	% Find where the array goes from non-zero to zero and vice versa
