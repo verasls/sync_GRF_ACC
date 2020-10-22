0cb34a06ae26345cbb0816479c44461c3ab248ff
Lucas <lucasdsveras@gmail.com>
2020-10-22 19:55:44 +0100

    Document function

--- code/functions/removeOffset.m
+++ code/functions/removeOffset.m
@@ -1,14 +1,25 @@
-function correctedVector = removeOffset(originalVector, offsetVector, sampFreq)
+function correctedSignal = removeOffset(originalSignal, offsetSignal, sampFreq)
+% REMOVEOFFSET subtracts an offset from the signal.
+%
+% Input arguments:
+% 	originalSignal: a double array with the signal to subtract the offset from.
+% 	offsetSignal: a double array with the offset signal.
+% 	sampFreq: a double scalar with the sample frequency in Hz.
+%
+% Output arguments:
+% 	correctedSignal: a double array with the offset subtracted from the
+% 	original signal.
+
 	% Remove end of file
-	lastLine = getEvalEnd(offsetVector);
-	offsetVector = offsetVector(1:lastLine);
+	lastLine = getEvalEnd(offsetSignal);
+	offsetSignal = offsetSignal(1:lastLine);
 
 	% Get offset time
 	timeOffset = 0:lastLine - 1;
 	timeOffset = timeOffset / sampFreq;
 
 	% Get offset to be removed and correct original file
-	offset = mean(offsetVector);
+	offset = mean(offsetSignal);
 
-	correctedVector = originalVector - offset;
+	correctedSignal = originalSignal - offset;
 end
