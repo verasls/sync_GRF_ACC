ca6f88e5c929eebffb8f1395d86818c8aff4fc57
Lucas <lucasdsveras@gmail.com>
2020-10-27 08:35:09 +0000

    Change peaks criteria

--- code/functions/syncSignals.m
+++ code/functions/syncSignals.m
@@ -128,11 +128,11 @@ ax = gca;
 ax.FontSize = 15;
 
 % Find peaks in the acceleration signal
-minHeight = 4 * mean(abs(accSignal));
+minHeight = 5 * mean(abs(accSignal));
 if ~isempty(regexp(grfFile, '\d_Jumps', 'once'))
 	minDist = 0.2 * sampFreqAcc;
 else
-	minDist = 3.5 * sampFreqAcc;
+	minDist = 4 * sampFreqAcc;
 end
 [pksAcc, pksAccIdx] = findpeaks(accSignal, 'MINPEAKHEIGHT', minHeight, ...
                                 'MINPEAKDISTANCE', minDist);
